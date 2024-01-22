00c5eeea8be53e1e2bedd7f0e347dfe4f1c333a2
Leonardo <leowebc@gmail.com>
2024-01-22 08:37:09 -0300

    LLibrary OK

--- test/index.php
+++ test/index.php
@@ -1,12 +1,12 @@
 <?php
 
-require  __DIR__ . "/lib_ext/autoload.php";
+require __DIR__ . "/../lib_ext/autoload.php";
 
 
 use Notification\Email;
 
 
-$novoEmail = new \Notification\Email();
+$novoEmail = new Email(2, "mail.gustavo.me", "sender@gustavoweb.me", "teste@123", "tls", 587 , "leowebc@gmail.com", "Equipe Leonardo" );
 $novoEmail->sendMail("Assunto de teste", "<p>Esse ´um e-mail <b> teste</b>!</p>", "gustavo@gustavoweb.me", "Gustavo Web",  "leowebc@gmail.com.br", "Leonardo");
 
 var_dump($novoEmail);
