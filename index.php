2c04fd037856264894357f3b97d291368287ac08
Leonardo <leowebc@gmail.com>
2024-01-22 06:44:32 -0300

    Commite Initial

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require  __DIR__ . "/lib_ext/autoload.php";
+
+
+use Notification\Email\Email;
+
+
+$novoEmail = new \Notification\Email();
+$novoEmail->sendMail("Assunto de teste", "<p>Esse ´um e-mail <b> teste</b>!</p>", "gustavo@gustavoweb.me", "Gustavo Web",  "leowebc@gmail.com.br", "Leonardo");
+
+var_dump($novoEmail);
